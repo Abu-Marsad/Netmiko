7acb0287b7bf938fa19db6a7ba1d84fc141b7a14
Abu-Marsad <marsadsandhu@hotmail.com>
2022-06-15 19:15:10 +0500

    Update backup_of_device_config.py
    Netmiko in Network automation! ❤️
    Last night tried another script. This script can back up the running configuration of the routers. The script takes the input of the IP Address of the required router and saves the running configuration in a .txt file
    In this lab!
    • Emulated Virtual Environment Next Generation (EVE-NG)
    • Devices: Cisco vIOS
    • Script is loaded on the Ubuntu server.
    • Library of Python: Netmiko

--- backup_of_device_config.py
+++ backup_of_device_config.py
@@ -1,1 +1,34 @@
+from netmiko import ConnectHandler
+import paramiko
+import netmiko
+import sys
+from getpass import getpass
+
+CSR = {
+    'device_type': 'cisco_ios',
+    'ip': input('Enter ip address: '),
+    'username': 'marsad',
+    'password': 'cisco'
+}
+
+net_connect = ConnectHandler(**CSR)
+hostname = net_connect.send_command('show run | i host')
+hostname.split(" ")
+hostname,device = hostname.split(" ")
+print ("Backing up " + device)
+
+filename = '/home/marsad/backups/' + device + '.txt'
+
+showrun = net_connect.send_command('show run')
+showvlan = net_connect.send_command('show vlan')
+showver = net_connect.send_command('show ver')
+log_file = open(filename, "a")
+log_file.write(showrun)
+log_file.write("\n")
+log_file.write(showvlan)
+log_file.write("\n")
+log_file.write(showver)
+log_file.write("\n")
+
+net_connect.disconnect()
 
